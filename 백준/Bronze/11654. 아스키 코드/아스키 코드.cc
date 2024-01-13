1a3038af1f7a4828b17d53b8195718f3c7523a08
Kim Hyeonwoo <ialskdji@gmail.com>
2024-01-13 15:24:20 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 1988 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char inputChar;
+    
+    scanf("%c", &inputChar);
+    
+    printf("%d", (int)inputChar);
+    
+    return 0;
+}
