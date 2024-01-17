0e504d2378e3bd823edb28a8ad8e23f1c7c6b4f5
Kim Hyeonwoo <ialskdji@gmail.com>
2024-01-17 20:21:11 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -0,0 +1,18 @@
+N, K = map(int, input().split())
+
+peopleList = []
+josephusPermutation = []
+currentIndex = 0
+
+for index in range(1, N + 1):
+    peopleList.append(index)
+
+while len(peopleList) > 0:
+    currentIndex = (currentIndex + K - 1) % len(peopleList)
+    peopleNumber = peopleList.pop(currentIndex)
+
+    josephusPermutation.append(peopleNumber)
+
+outputString = ", ".join(map(str, josephusPermutation))
+
+print(f"<{outputString}>")
