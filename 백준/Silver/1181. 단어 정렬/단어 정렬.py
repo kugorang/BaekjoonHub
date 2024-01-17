962a151df0c5e5eaca260b1d69505b25d8a3845e
Kim Hyeonwoo <ialskdji@gmail.com>
2024-01-17 15:15:12 +0900

    [Silver V] Title: 단어 정렬, Time: 88 ms, Memory: 35732 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -1,12 +1,10 @@
 import sys
 
-# N = int(sys.stdin.readline())
-N = int(input())
+N = int(sys.stdin.readline())
 resultList = []
 
 for _ in range(N):
-    # resultList.append(sys.stdin.readline())
-    resultList.append(input())
+    resultList.append(sys.stdin.readline().strip())
 
 resultList = list(set(resultList))
 resultList.sort(key = lambda x: (len(x), x))
