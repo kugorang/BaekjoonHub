b6b52bcba94782263d30d3d46e142f82a869684c
Kim Hyeonwoo <ialskdji@gmail.com>
2024-01-18 10:15:19 +0900

    [Bronze I] Title: 더하기 사이클, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
@@ -1,10 +1,13 @@
+def plusCycle(N):
+    return  (N % 10 * 10) + (N // 10 + N % 10) % 10
+
 N = int(input())
 
-currentNumber = (N % 10 * 10) + (N // 10 + N % 10) % 10
+currentNumber = plusCycle(N)
 loopCount = 1
 
 while currentNumber != N:
-    currentNumber = (currentNumber % 10 * 10) + (currentNumber // 10 + currentNumber % 10) % 10
+    currentNumber = plusCycle(currentNumber)
     loopCount += 1
     
 print(loopCount)
