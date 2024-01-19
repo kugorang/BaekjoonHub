5a0258c7152254e2d4dd2218cb0fc009b06131e4
Kim Hyeonwoo <ialskdji@gmail.com>
2024-01-19 14:04:21 +0900

    [Silver II] Title: DFS와 BFS, Time: 72 ms, Memory: 34088 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,27 +1,29 @@
 import sys
 from collections import deque
 
-# graph, visited는 전역변수로 선언
-def dfs(graph, v, visited):
-    visited[v] = True
-    print(v, end=' ')
-
-    for i in graph[v]:
-        if not visited[i]:
-            dfs(graph, i, visited)
-
 # def dfs(graph, v, visited):
-#     stack = list([v])
 #     visited[v] = True
-    
-#     while stack:
-#         v = stack.pop()
-#         print(v, end=' ')
-        
-#         for i in graph[v]:
-#             if not visited[i]:
-#                 stack.append(i)
-#                 visited[i] = True
+#     print(v, end=' ')
+
+#     for i in graph[v]:
+# if not visited[i]:
+#             dfs(graph, i, visited)
+
+
+def dfs(graph, v, visited):
+    stack = deque([v])
+
+    while stack:
+        current = stack.pop()
+
+        if not visited[current]:
+            visited[current] = True
+            print(current, end=" ")
+
+            for i in reversed(graph[current]):
+                if not visited[i]:
+                    stack.append(i)
+
 
 def bfs(graph, start, visited):
     queue = deque([start])
@@ -29,12 +31,13 @@ def bfs(graph, start, visited):
 
     while queue:
         v = queue.popleft()
-        print(v, end=' ')
+        print(v, end=" ")
 
         for i in graph[v]:
             if not visited[i]:
                 queue.append(i)
                 visited[i] = True
+
 
 # N: 정점의 개수, M: 간선의 개수, V: 시작 정점 번호
 N, M, V = map(int, sys.stdin.readline().split())
