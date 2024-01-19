e006b992ff5529511f7dcdb0637aed58e9295d0c
Kim Hyeonwoo <ialskdji@gmail.com>
2024-01-19 22:56:38 +0900

    [Silver III] Title: 바이러스, Time: 40 ms, Memory: 31132 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,3 +1,5 @@
+import sys
+
 def dfs(graph, start, visited):
     visited[start] = True
     virusCount = 1
@@ -10,13 +12,14 @@ def dfs(graph, start, visited):
 
     return virusCount
 
-totalComputerCount = int(input())
-computerPairCount = int(input())
+totalComputerCount = int(sys.stdin.readline())  # 컴퓨터 수
+computerPairCount = int(sys.stdin.readline())   # 컴퓨터 쌍의 수
 
+# 네트워크 (그래프) 설정
 network = [ [] for _ in range(totalComputerCount + 1) ]
 
-for _ in range(computerPairCount):
-    computer1, computer2 = map(int, input().split())
+for _ in range(computerPairCount):  # 컴퓨터 번호의 쌍을 한 줄씩 처리
+    computer1, computer2 = map(int, sys.stdin.readline().split())
 
     network[computer1].append(computer2)
     network[computer2].append(computer1)
