80e6a9bd06c285752a8e620502f6144af208e8ee
Kim Hyeonwoo <ialskdji@gmail.com>
2024-01-27 14:04:00 +0900

    [Silver III] Title: 01타일, Time: 648 ms, Memory: 102856 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -0,0 +1,13 @@
+import sys
+
+N = int(sys.stdin.readline())
+
+tileCount = [ [] for _ in range(N + 1) ]
+
+tileCount[0] = 1
+tileCount[1] = 1
+
+for index in range(2, N + 1) :
+    tileCount[index] = (tileCount[index - 1] + tileCount[index - 2]) % 15746
+
+print(tileCount[N])
