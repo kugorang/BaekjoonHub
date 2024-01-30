3c4a3a9b8f2e1bd7bb5ef2fe79742a164ed15db6
Kim Hyeonwoo <ialskdji@gmail.com>
2024-01-30 19:57:34 +0900

    [Silver I] Title: 신입 사원, Time: 3516 ms, Memory: 44580 KB -BaekjoonHub

---
+++ 백준/Silver/1946. 신입 사원/신입 사원.py
@@ -0,0 +1,25 @@
+import sys
+
+T = int(sys.stdin.readline())
+
+for _ in range(T):
+    applyPeopleNum = int(sys.stdin.readline())
+    appliers = []
+
+    for _ in range(applyPeopleNum):
+        documentRank, interviewRank = map(int, sys.stdin.readline().split())
+        appliers.append((documentRank, interviewRank))
+
+    # 서류 심사 순위로 정렬
+    appliers.sort()
+
+    # 채용할 수 있는 최대 인원 수
+    maxHire = 1  # 서류 심사 1위는 무조건 선발
+    bestInterviewRank = appliers[0][1]  # 서류 심사 1위의 면접 순위
+
+    for i in range(1, applyPeopleNum):
+        if appliers[i][1] < bestInterviewRank:
+            maxHire += 1
+            bestInterviewRank = appliers[i][1]
+
+    print(maxHire)
