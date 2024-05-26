5a471a86502079fd92571d4b1b96c260179748ae
Kim Hyeonwoo <ialskdji@gmail.com>
2024-05-26 23:49:38 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string nickname;
+    
+    cin >> nickname;
+    
+    cout << nickname << "??!";
+    
+    return 0;
+}
