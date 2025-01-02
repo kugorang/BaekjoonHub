7d7e5312ac40a75a2d60ae23ae422de7686a8e1f
Kim Hyeonwoo <ialskdji@gmail.com>
2025-01-02 23:12:37 +0900

    [level 0] Title: 세균 증식, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/120910. 세균 증식/세균 증식.cpp
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.cpp
@@ -4,13 +4,5 @@
 using namespace std;
 
 int solution(int n, int t) {
-    vector<int> cells;
-    cells.reserve(t);
-    
-    cells.push_back(n);
-    
-    for (int i = 1; i <= t; ++i)
-        cells.push_back(cells[i - 1] * 2);
-    
-    return cells[t];
+    return n << t;
 }
