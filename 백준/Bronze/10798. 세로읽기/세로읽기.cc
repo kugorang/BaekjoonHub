91098c77a3583d0abea0a8db7595b7dea27b457f
Kim Hyeonwoo <ialskdji@gmail.com>
2025-01-09 22:54:51 +0900

    [Bronze I] Title: 세로읽기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    char arr[5][16] = { '\0', };
+    
+    for (int row = 0; row < 5; ++row)
+    {
+        int col = 0;
+        string inputStr;
+        cin >> inputStr;
+        
+        while (inputStr[col])
+        {
+            arr[row][col] = inputStr[col];
+            ++col;
+        }
+    }
+
+    for (int col = 0; col < 15; ++col)
+        for (int row = 0; row < 5; ++row)
+            if (arr[row][col])
+                cout << arr[row][col];
+
+    return 0;
+}
