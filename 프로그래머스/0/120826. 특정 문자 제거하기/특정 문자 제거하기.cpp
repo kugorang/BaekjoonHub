e427c28dfc07cd4a98356db94ce6cd3032c74f59
Kim Hyeonwoo <ialskdji@gmail.com>
2025-01-23 10:21:40 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, string letter) {
+    string answer = "";
+    
+    for (char ch : my_string)
+    {
+        if (ch == letter[0])
+            continue;
+        
+        answer.push_back(ch);
+    }
+    
+    return answer;
+}
