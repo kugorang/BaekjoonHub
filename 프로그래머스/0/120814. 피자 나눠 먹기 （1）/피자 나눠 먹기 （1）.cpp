f9f1e892d6ad00b65aedfabf968c7792efb67f7d
Kim Hyeonwoo <ialskdji@gmail.com>
2025-01-29 13:24:31 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.04 ms, Memory: 4.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = n / 7 + 1;
+    
+    if (n % 7 == 0)
+        --answer;
+
+    return answer;
+}
