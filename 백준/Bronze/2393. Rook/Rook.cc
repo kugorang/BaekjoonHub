1b22ec05eead8178d3925639fed625ad19b7bf2c
Kim Hyeonwoo <ialskdji@gmail.com>
2025-02-01 23:53:40 +0900

    [Bronze V] Title: Rook, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "  ___  ___  ___" << endl 
+        << "  | |__| |__| |" << endl
+        << "  |           |" << endl
+        << "   \\_________/" << endl
+        << "    \\_______/" << endl
+        << "     |     |" << endl
+        << "     |     |" << endl
+        << "     |     |" << endl
+        << "     |     |" << endl
+        << "     |_____|" << endl
+        << "  __/       \\__" << endl
+        << " /             \\" << endl
+        << "/_______________\\";
+
+    return 0;
+}
