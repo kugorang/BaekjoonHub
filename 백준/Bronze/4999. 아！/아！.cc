80607b22b41733a93047f43c440657b773d7715d
Kim Hyeonwoo <ialskdji@gmail.com>
2025-02-09 09:38:55 +0900

    [Bronze V] Title: 아!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/4999. 아！/아！.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string str1, str2;
+    cin >> str1 >> str2;
+    
+    if (str1.length() >= str2.length())
+        cout << "go";
+    else
+        cout << "no";
+    
+    return 0;
+}
