5d5e6b250d871152a0e60f79d34e0af40822870f
Kim Hyeonwoo <ialskdji@gmail.com>
2025-02-27 10:41:32 +0900

    [Bronze V] Title: 가희와 4시간의 벽 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32775. 가희와 4시간의 벽 1/가희와 4시간의 벽 1.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int S, F;
+    
+    cin >> S >> F;
+    
+    cout << (S > F ? "flight" : "high speed rail");
+    
+    return 0;
+}
