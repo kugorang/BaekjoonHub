e97b051aee4f5cac2e64ebeb4570fdbdb65f29aa
Kim Hyeonwoo <ialskdji@gmail.com>
2025-03-06 22:32:57 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    
+    cout << (N == 0 ? "YONSEI" : "Leading the Way to the Future");
+    
+    return 0;
+}
