4abc67009cf1dafafd3332d2973ec05f9130200a
Kim Hyeonwoo <ialskdji@gmail.com>
2025-03-23 11:44:02 +0900

    [Bronze V] Title: CAPS, Time: 32 ms, Memory: 4916 KB -BaekjoonHub

---
+++ 백준/Bronze/15000. CAPS/CAPS.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    string str;
+    cin >> str;
+    
+    transform(str.begin(), str.end(), str.begin(), ::toupper);
+        
+    cout << str;
+    
+    return 0;
+}
