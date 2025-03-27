a803e95f636189c4f7bd60e498fe2a49e00838ee
Kim Hyeonwoo <ialskdji@gmail.com>
2025-03-27 19:39:31 +0900

    [Bronze V] Title: 자동완성, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char ch;
+    cin >> ch;
+    
+    if (ch == 'N' || ch == 'n')
+        cout << "Naver D2";
+    else
+        cout << "Naver Whale";
+    
+    return 0;
+}
