bb4c3ec4fb958911c58835a66ee074a155eb1a4d
Kim Hyeonwoo <ialskdji@gmail.com>
2025-03-31 08:47:26 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 0.03 ms, Memory: 4.02 MB -BaekjoonHub

--- 프로그래머스/0/181852. 뒤에서 5등 위로/README.md
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/README.md
@@ -1,10 +1,10 @@
 # [level 0] 뒤에서 5등 위로 - 181852 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181852) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181852?language=cpp) 
 
 ### 성능 요약
 
-메모리: 4.18 MB, 시간: 0.02 ms
+메모리: 4.02 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 31일 08:45:59
+2025년 03월 31일 08:47:25
 
 ### 문제 설명
 
--- 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.cpp
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.cpp
@@ -7,12 +7,5 @@ using namespace std;
 vector<int> solution(vector<int> num_list) {
     sort(num_list.begin(), num_list.end());
     
-    int loopEnd = num_list.size();
-    vector<int> answer;
-    answer.reserve(loopEnd - 5);
-    
-    for (int i = 5; i < loopEnd; ++i)
-        answer.push_back(num_list[i]);
-    
-    return answer;
+    return vector<int>(num_list.begin() + 5, num_list.end());
 }
