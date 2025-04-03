a107bb4ebbaa6680185a1bcedbd0bc1e7c1c4302
Kim Hyeonwoo <ialskdji@gmail.com>
2025-04-03 10:28:30 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> num_list) {
+    int num_list_size = num_list.size();
+    
+    for (int i = 0; i < num_list_size; ++i)
+    {
+        if (num_list[i] < 0)
+            return i;
+    }
+    
+    return -1;
+}
