1d5dcb782805f3ecf1009a7011c7e90eeb027e2a
Kim Hyeonwoo <ialskdji@gmail.com>
2025-04-03 10:20:06 +0900

    [Bronze V] Title: 火曜日 (Tuesday), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31611. 火曜日 （Tuesday）/火曜日 （Tuesday）.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    cin >> x;
+    
+    if (x % 7 == 2)
+        cout << 1;
+    else
+        cout << 0;
+
+    return 0;
+}
