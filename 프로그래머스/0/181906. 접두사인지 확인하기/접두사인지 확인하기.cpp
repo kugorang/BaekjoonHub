50ee397a9890614baa55107beef09e28c45023a7
Kim Hyeonwoo <ialskdji@gmail.com>
2025-04-04 11:56:59 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.01 ms, Memory: 4.06 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.cpp
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.cpp
@@ -4,11 +4,5 @@
 using namespace std;
 
 int solution(string my_string, string is_prefix) {
-    int is_prefix_length = is_prefix.length();
-    
-    for (int i = 0; i < is_prefix_length; ++i)
-        if (is_prefix[i] != my_string[i])
-            return 0;
-
-    return 1;
+    return my_string.find(is_prefix) == 0 ? 1 : 0;
 }
