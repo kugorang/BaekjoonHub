8e477419eb5c36deab60d216cd8144b3393478ed
Kim Hyeonwoo <ialskdji@gmail.com>
2025-04-26 14:10:57 +0900

    [Bronze V] Title: Mathematics, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26545. Mathematics/Mathematics.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    
+    int sum = 0;
+    
+    for (int i = 0; i < n; ++i)
+    {
+        int num;
+        cin >> num;
+        
+        sum += num;
+    }
+    
+    cout << sum;
+    
+    return 0;
+}
