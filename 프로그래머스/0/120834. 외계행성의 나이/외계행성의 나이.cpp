0b8b4d669009fc050db9e026945a17a92739b60a
Kim Hyeonwoo <ialskdji@gmail.com>
2025-04-28 21:22:24 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.cpp
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.cpp
@@ -1,6 +1,5 @@
 #include <string>
 #include <vector>
-#include <cstdlib>
 
 using namespace std;
 
@@ -8,7 +7,7 @@ string solution(int age) {
     string answer = to_string(age);
     
     for (auto &element : answer)
-        element = 'a' + (element - '0');
+        element += 'a' - '0';
 
     return answer;
 }
