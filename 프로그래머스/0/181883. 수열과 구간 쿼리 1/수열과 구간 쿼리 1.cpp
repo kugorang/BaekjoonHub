2f7dea1ff3d0aaf9098f1a5e8a4809fc463ed615
Kim Hyeonwoo <ialskdji@gmail.com>
2025-05-04 13:32:51 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 0.29 ms, Memory: 4.12 MB -BaekjoonHub

--- 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.cpp
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 vector<int> solution(vector<int> arr, vector<vector<int>> queries)
 {    
-    for (auto query : queries)
+    for (const auto &query : queries)
         for (int i = query[0]; i <= query[1]; ++i)
             ++arr[i];
     
