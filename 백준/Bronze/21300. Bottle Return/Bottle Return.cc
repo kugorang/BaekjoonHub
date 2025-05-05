2c3ae43fec0047e74963ee38c8227d78c4d82c56
Kim Hyeonwoo <ialskdji@gmail.com>
2025-05-05 18:16:50 +0900

    [Bronze V] Title: Bottle Return, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/21300. Bottle Return/Bottle Return.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n1, n2, n3, n4, n5, n6;
+    
+    cin >> n1 >> n2 >> n3 >> n4 >> n5 >> n6;
+    
+    cout << (n1 + n2 + n3 + n4 + n5 + n6) * 5;
+    
+    return 0;
+}
