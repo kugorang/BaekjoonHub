e266e77b0699739e5553477235571706ed39befa
Kim Hyeonwoo <ialskdji@gmail.com>
2025-05-18 06:17:19 +0900

    [Bronze V] Title: Rats, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18301. Rats/Rats.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int n1, n2, n12;
+    
+    cin >> n1 >> n2 >> n12;
+    
+    cout << floor((n1 + 1) * (n2 + 1) / (n12 + 1) - 1);
+    
+    return 0;
+}
