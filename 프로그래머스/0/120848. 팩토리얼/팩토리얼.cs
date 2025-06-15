e5d21a37f9a3080e60111d0e1e99ac678756a38e
Kim Hyeonwoo <ialskdji@gmail.com>
2025-06-15 19:33:36 +0900

    [level 0] Title: 팩토리얼, Time: 0.16 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.cs
@@ -0,0 +1,20 @@
+using System;
+
+public class Solution {
+    public int solution(int n)
+    {
+        int result = 1;
+        int i = 2;
+        
+        while (i <= n)
+        {
+            if (result * i > n)
+                break;
+            
+            result *= i;
+            ++i;
+        }
+        
+        return i - 1;
+    }
+}
