7db5a04e66bd009f346fe8fe27511f2c3e68ecd4
Kim Hyeonwoo <ialskdji@gmail.com>
2025-06-19 20:30:59 +0900

    [Bronze IV] Title: Celebrity jeopardy, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/3765. Celebrity jeopardy/Celebrity jeopardy.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    while (!cin.eof())
+    {
+        string str;
+        getline(cin, str);
+
+        cout << str << endl;
+    }
+    
+    return 0;
+}
