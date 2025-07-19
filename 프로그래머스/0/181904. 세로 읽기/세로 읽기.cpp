3ed243b7154fb59c135356e82a695f7b0b5b7b04
Kim Hyeonwoo <ialskdji@gmail.com>
2025-07-19 21:25:45 +0900

    [level 0] Title: 세로 읽기, Time: 0.01 ms, Memory: 4.06 MB -BaekjoonHub

---
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, int m, int c) {
+    string answer = "";
+    int my_string_len = my_string.length();
+    
+    for (int i = 0; i < my_string_len; i += m)
+        answer.push_back(my_string[i + c - 1]);
+    
+    return answer;
+}
