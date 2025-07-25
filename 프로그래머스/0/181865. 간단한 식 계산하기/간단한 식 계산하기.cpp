ecdb3137870307fd2a5beddec9c565b925504954
Kim Hyeonwoo <ialskdji@gmail.com>
2025-07-25 21:55:07 +0900

    [level 0] Title: 간단한 식 계산하기, Time: 0.03 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.cpp
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.cpp
@@ -1,6 +1,5 @@
 #include <string>
 #include <vector>
-#include <iostream>
 
 using namespace std;
 
@@ -19,19 +18,19 @@ string parse(const string &s, int &i)
 
 int solution(string binomial) {
     int answer = 0;
-    string a, op, b;
     int i = 0;
-    
+    string a, op, b;
+
     a = parse(binomial, i);    
     op = parse(binomial, i);    
     b = parse(binomial, i);
-    
+
     if (op == "+")
         answer = stoi(a) + stoi(b);
     else if (op == "-")
         answer = stoi(a) - stoi(b);
     else if (op == "*")
         answer = stoi(a) * stoi(b);
-    
+
     return answer;
 }
